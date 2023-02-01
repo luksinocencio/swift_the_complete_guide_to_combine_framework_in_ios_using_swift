2764781319a15008076bb3c567a0224c0515deb0
luksinocencio <luksinocencio@gmail.com>
2023-02-01 01:27:12 -0300

    *

---
+++ 07_combine_for_networking/NetworkingCombineApp/NetworkingCombineApp/Services/WebService.swift
@@ -0,0 +1,16 @@
+import Combine
+import Foundation
+
+class WebService {
+    func getPosts() -> AnyPublisher<[Post], Error> {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+            fatalError("Invalid URL")
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map { $0.data }
+            .decode(type: [Post].self, decoder: JSONDecoder())
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+}
